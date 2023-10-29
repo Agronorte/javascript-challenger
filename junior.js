931747c738f5a0642fba727f3544ead84195fbf8
Raimones Barros <raimonesrg3@gmail.com>
2023-10-28 22:21:21 -0300

    feat: test completed successfully

--- junior.js
+++ junior.js
@@ -1,26 +1,32 @@
 //Crie uma função que receba uma string e retorne a mesma string, mas com todas as letras em maiúsculas.
-const toUpperCase = (str) => {};
+const toUpperCase = (str) => str.toUpperCase();
 
 //Escreva uma função que recebe duas strings como argumento e concatena-as em uma única string com um espaço entre elas.
-const concatStrings = (str1, str2) => {};
+const concatStrings = (str1, str2) => `${str1}${str2}`;
 
 //Crie uma função que recebe uma string como argumento e retorna o número de caracteres nessa string.
-const numberString = (str) => {};
+const numberString = (str) => str.length;
 
 //Crie uma função que receba uma palavra, e uma frase. Você deve verificar se existe esta palavra nesta frase e retornar true ou false.
-const existKeyword = (keyword, str) => {};
+const existKeyword = (keyword, str) => {
+  const trasformStrToLowerCase = str.toLowerCase();
+  const transformKeywordToLowerCase = keyword.toLowerCase();
+
+  return trasformStrToLowerCase.includes(transformKeywordToLowerCase);
+};
 
 //Escreva uma função que recebe um array de números como argumento e retorna a soma de todos os números no array.
-const sumAllItemsInArray = (arr) => {};
+const sumAllItemsInArray = (arr) =>
+  arr.reduce((acc, current) => acc + current, 0);
 
 //Você receberá uma lista com nomes de usuarios, verifique se um usuario existe neste array e retorne true ou false.
-const verifyUserInArray = (strArr, str) => {};
+const verifyUserInArray = (strArr, str) => strArr.includes(str);
 
 //Escreva uma função que recebe dois números como argumento e retorna a soma deles.
-const sumNumber = (num1, num2) => {};
+const sumNumber = (num1, num2) => num1 + num2;
 
 //Crie uma função que recebe um número como argumento e verifica se ele é par ou ímpar retornando  a string "par" ou "impar"
-const verifyNumberIsPairOrOdd = (num) => {};
+const verifyNumberIsPairOrOdd = (num) => (num % 2 === 0 ? "par" : "impar");
 
 module.exports = {
   toUpperCase,
